793613be1a8cd82b20022af29d7b8ab1e1034b48
Steven_Laptop <test@test.test>
2017-04-18 01:33:40 +0200

    Testing git

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,20 +1,86 @@
 /**
  * Created by Home on 11.04.2017.
  */
-public class GameBoard {
-    private Field[][] grid = new Field[3][3];
-    int turn;
-    
+class GameBoard {
+    private static int FIELDSIZE = 3;
+    private int currentRow;
+    private int currentColumn;
+    private int[][] grid = new int[FIELDSIZE][FIELDSIZE];
+    private int currentPlayer = 2; //1=x;2=O
+    private int movecounter = 0;
 
-    public int checkForWinner(){
-        for (int i = 0; i < 3; i++) {
-            if(grid[1][i] == grid[2][i] && grid[2][i] == grid[3][i] && grid[3][i] != 0) return grid[3][i];
-            if(grid[i][1] == grid[i][2] && grid[i][2] == grid[i][3] && grid[i][3] != 0) return grid[i][3];
-        }
-        if(grid[1][1] == grid[2][2] && grid[2][2] == grid[3][3] && grid[3][3] != 0) return grid[3][3];
-        if(grid[3][1] == grid[2][2] && grid[2][2] == grid[1][3] && grid[1][3] != 0) return grid[1][3];
-        if (turn >= 9) return 3;
-        return 0;
+    GameBoard() {
     }
 
+    void move(int x, int y){
+
+        if(grid[x][y] != 0){
+            System.out.println("invalid move");
+            return;
+        }
+        currentColumn = y;
+        currentRow = x;
+        grid[x][y] = currentPlayer;
+        movecounter++;
+        drawBoard();
+    }
+
+    int findWinner(){
+        if(movecounter >= FIELDSIZE*FIELDSIZE) return 3;
+        else if(checkRow() || checkColumn() || checkDiagonal() || checkAntidiagonal()) return currentPlayer;
+        else return 0;
+    }
+
+    void swapPlayer(){
+        if(currentPlayer == 1) currentPlayer = 2;
+        else currentPlayer = 1;
+    }
+
+    private boolean checkRow() {
+        for (int i = 0; i < FIELDSIZE; i++) {
+            if (grid[currentRow][i] != currentPlayer) return false;
+        }
+        return true;
+    }
+
+    private boolean checkColumn() {
+        for (int i = 0; i < FIELDSIZE; i++) {
+            if (grid[i][currentColumn] != currentPlayer) return false;
+        }
+        return true;
+    }
+
+    private boolean checkDiagonal(){
+        for(int i = 0; i < FIELDSIZE; i++){
+            if(grid[i][i] != currentPlayer) return false;
+        }
+        return true;
+    }
+
+    private boolean checkAntidiagonal(){
+        for(int i = 0; i < FIELDSIZE; i++){
+            if(grid[FIELDSIZE-i-1][i] != currentPlayer) return false;
+        }
+        return true;
+    }
+
+    private void drawBoard(){
+        for(int i = 0; i < FIELDSIZE; i++){
+            for(int j = 0; j < FIELDSIZE; j++) {
+                drawCell(grid[i][j]);
+                if (j >= 2) System.out.println();
+                if (j < 2) System.out.print(" | ");
+
+            }
+            if(i < FIELDSIZE-1)System.out.println("----------");
+        }
+    }
+
+    private void drawCell(int x){
+        char c;
+        if(x == 1) c='X';
+        else if(x == 2) c='O';
+        else c=' ';
+        System.out.print(c);
+    }
 }
--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,10 +1,21 @@
 /**
  * Created by Home on 11.04.2017.
  */
+import java.util.Scanner;
+
 public class Gameplay {
 
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        GameBoard myGameBoard = new GameBoard();
 
-    public void(String[] args){
+        do {
+            myGameBoard.swapPlayer();
+            System.out.println("Enter X then Y");
+            myGameBoard.move(scn.nextInt() , scn.nextInt());
+        }
+        while (myGameBoard.findWinner()==0);
 
+        System.out.printf("Winner : %d",myGameBoard.findWinner());
     }
 }
